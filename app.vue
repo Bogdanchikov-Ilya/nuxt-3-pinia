507d9a5a0ce326674964f53cdef789f9a40bd67e
Илья Богданчиков <test200373@gmail.com>
2023-03-21 12:15:44 +0300

    example: work pinia + vue on option api without script setup

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col items-center">
+    <span class="text-cyan-900 font-bold text-xl">{{counterStore.mainCount}}</span>
+    <br>
+    <button @click="counterStore.countIncrement()" class="rounded-xl border-2 p-0.5 w-1/4">add</button>
+  </div>
+</template>
+<script>
+import { mapState, mapActions, mapStores } from 'pinia'
+import {useCounterStore} from "~/stores/counter";
+export default {
+  computed: {
+    // note we are not passing an array, just one store after the other
+    // each store will be accessible as its id + 'Store'
+    ...mapStores(useCounterStore)
+  },
+  methods: {
+    test() {
+      console.log(this.counterStore)
+      console.log(this.counterStore.mainCount);
+    }
+  }
+}
+</script>
